da991dc398d47adc101ba4418ce693a56bdf7deb
lthuynhnhu <huynhnhu6022@gmail.com>
2026-04-21 15:59:49 +0700

    Sua loi lan 10

--- routers/ChamCong.js
+++ routers/ChamCong.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const chamCongModel = require('../models/ChamCsong');
+const chamCongModel = require('../models/ChamCong');
 
 
 router.get('/', async (req, res) => {
